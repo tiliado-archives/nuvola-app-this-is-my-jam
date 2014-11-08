b3ec23d29ff332e3b77b1d9970c44903408c0521
Jan Vlnas <git@jan.vlnas.cz>
2014-11-08 15:51:36 +0100

    Read player state

--- integrate.js
+++ integrate.js
@@ -34,13 +34,131 @@
     var PlaybackState = Nuvola.PlaybackState;
     var PlayerAction = Nuvola.PlayerAction;
 
+    var clickOnElement = Nuvola.clickOnElement;
+
     // Create new WebApp prototype
     var WebApp = Nuvola.$WebApp();
+
+    // l10n
+    var C_ = Nuvola.Translate.pgettext;
+    var ngettext = Nuvola.Translate.ngettext;
+
+    //////////////////////// TMIJ bindings
+
+    /**
+     * Get an element of a given type from the document.
+     * Element IDs are contained within a closure to prevent usage outside functions' scope.
+     * @param  {string} type Control type, e.g. prev, next, play, like…
+     * @return {Element}
+     */
+    var getElement = (function elementSelector() {
+        var ELEM_IDS = {
+            prev: "backwards",
+            next: "forwards",
+            title: "track-title",
+            artist: "artist-name",
+            play: "playPause",
+            like: "controlLike",
+            playAll: "playAllJams"
+        };
+
+        return function(type) {
+            return document.getElementById(ELEM_IDS[type]);
+        };
+    })();
+
+    /**
+     * Get a state for a playlist's prev and next buttons.
+     * @return {{prev: bool, next: bool}}
+     */
+    var getPlaylistState = function() {
+        var ret = {};
+        ["prev", "next"].forEach(function(type) {
+            ret[type] = !getElement(type).hasAttribute("disabled");
+        });
+        return ret;
+    };
+
+    /**
+     * Return an URL of the image of (hopefully) currently playing track
+     * TODO: store the first found album art with the currently playing track,
+     *       so the visiting the profile page does not replace a correct album art
+     *       - OTOH, if I am playing a playlist and I am on the profile page, the incorrect
+     *       art will be loaded and stored
+     **/
+    var getArtLocation = function() {
+        var img = null;
+        // On Playlist page, things are easy
+        img = document.querySelector(".blackHole.playing img");
+        if (img) {
+            return img.getAttribute("data-thumb");
+        }
+        // Let's try a profile page
+        img = document.querySelector("#jamHolder img");
+        if (img) {
+            return img.src;
+        }
+
+        // No can do
+        return null;
+    };
+
+
+    /**
+     * Get play state depending on the play button
+     * @return {PlaybackState}
+     */
+    var getState = function() {
+        var el = getElement("play");
+
+        if (!el)
+        {
+            return PlaybackState.UNKNOWN;
+        }
+
+        if (el.className.match(/playing$/))
+        {
+            return PlaybackState.PLAYING;
+        }
+        else if (el.className.match(/paused$/))
+        {
+            return PlaybackState.PAUSED;
+        }
+
+        return PlaybackState.UNKNOWN;
+    };
+
+    var doPlay = function() {
+        var play = getElement("play");
+        if (getState() !== PlaybackState.UNKNOWN) {
+            clickOnElement(play);
+            return true;
+        }
+        var playAll = getElement("playAll");
+        if (playAll) {
+            clickOnElement(playAll);
+            return true;
+        }
+        return false;
+    };
+    //////////////////////// END TMIJ bindings
+
+    WebApp._onInitAppRunner = function(emitter)
+    {
+        Nuvola.WebApp._onInitAppRunner.call(this, emitter);
+
+    };
+
 
     // Initialization routines
     WebApp._onInitWebWorker = function(emitter)
     {
         Nuvola.WebApp._onInitWebWorker.call(this, emitter);
+
+        this.playbackState = PlaybackState.UNKNOWN;
+
+        // Connect handler for signal ActionActivated
+        Nuvola.actions.connect("ActionActivated", this);
 
         var state = document.readyState;
         if (state === "interactive" || state === "complete")
@@ -56,27 +174,50 @@
     // Page is ready for magic
     WebApp._onPageReady = function()
     {
-        // Connect handler for signal ActionActivated
-        Nuvola.actions.connect("ActionActivated", this);
-
         // Start update routine
         this.update();
     };
 
     // Extract data from the web page
-    WebApp.update = function()
-    {
+    WebApp.update = function() {
+        // Default values
+        var state = PlaybackState.UNKNOWN;
         var track = {
             title: null,
             artist: null,
-            album: null,
-            artLocation: null
+            artLocation: null,
+            album: null
         };
+        var canPrev = false;
+        var canNext = false;
 
+        state = getState();
+        player.setPlaybackState(state);
+
+        try {
+            track.title = getElement("title").textContent;
+            track.artist = getElement("artist").textContent;
+            track.artLocation = getArtLocation();
+        }
+        catch (ex) {
+            console.log(ex);
+            track.song = track.artist = track.artLocation = null;
+        }
         player.setTrack(track);
-        player.setPlaybackState(PlaybackState.UNKNOWN);
 
-        // Schedule the next update
+        try {
+            var playlist = getPlaylistState();
+            canPrev = playlist.prev;
+            canNext = playlist.next;
+        }
+        catch (ex) {
+            console.log(ex);
+        }
+        // Update actions
+        player.setCanGoPrev(canPrev);
+        player.setCanGoNext(canNext);
+
+        // Schedule update
         setTimeout(this.update.bind(this), 500);
     };
 
